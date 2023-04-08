49d3ee9ad3e06bc03ad96f437228036684f3d2b8
youjin-10 <105618795+youjin-10@users.noreply.github.com>
2023-04-08 19:49:47 +0900

    add framer motion

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -7,7 +7,7 @@ import summarizeCase from "@/utils/summarizeCase";
 import Lawyers from "@/components/Lawyers";
 import QnaAccordion from "@/components/QnaAccordion";
 import { KakaoChannel } from "@/components/KakaoChannel";
-
+import { motion } from "framer-motion";
 // const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
@@ -19,37 +19,12 @@ export default function Home() {
       </Head>
 
       <section className="flex flex-1 max-w-[800px] mx-auto flex-col items-center justify-center text-center px-4 mt-12 sm:mt-20">
-        <h2 className="text-3xl sm:text-4xl md:text-6xl font-bold mb-10">
-          <span>당신을 위한 </span>
-          <br />
-          <span>빠른 무료법률상담 서비스</span>
-        </h2>
-        <div className="h-[200px] sm:min-h-[450px] w-full relative">
-          <Image
-            src="/test-ask.png"
-            alt="user asking question"
-            width={320}
-            height={320}
-            className="absolute top-10 right-10 sm:right-20 md:right-14 w-36 sm:w-52 md:w-80 transition-all duration-700 ease-out transform hover:scale-110"
-          />
-          <Image
-            src="/test-answer.png"
-            alt="law-gpt answering question"
-            width={320}
-            height={320}
-            className="absolute top-28 sm:top-32 md:top-40 left-10 sm:left-20 md:left-14 w-36 sm:w-52 md:w-80 transition-all duration-700 ease-out transform hover:scale-110"
-          />
-        </div>
+        <MainTitle />
+        <MainChatImages />
 
         {/* <KakaoChannel /> */}
 
-        <div className="text-xl md:text-3xl mt-28 mb-48">
-          <div>
-            법률서비스 이용자들에게 <strong>생성형AI</strong>를 이용한
-          </div>
-          <div>무료법률상담 솔루션을 제공하고</div>
-          <div>이를 바탕으로 전문 변호사의 정보를 제공합니다.</div>
-        </div>
+        <MainIntro />
 
         <Lawyers />
         <QnaAccordion />
@@ -59,3 +34,87 @@ export default function Home() {
     </>
   );
 }
+
+const MainChatImages = () => {
+  const variants = {
+    hidden: { opacity: 0 },
+    visible: { opacity: 1 },
+  };
+
+  return (
+    <div className="h-[200px] sm:min-h-[450px] w-full relative">
+      <motion.div
+        initial="hidden"
+        animate="visible"
+        variants={variants}
+        transition={{ duration: 1, delay: 0.5 }}
+      >
+        <Image
+          src="/test-ask.png"
+          alt="user asking question"
+          width={320}
+          height={320}
+          className="absolute top-10 right-10 sm:right-20 md:right-14 w-36 sm:w-52 md:w-80 transition-all duration-700 ease-out transform hover:scale-110"
+        />
+      </motion.div>
+      <motion.div
+        initial="hidden"
+        animate="visible"
+        variants={variants}
+        transition={{ duration: 1, delay: 0.8 }}
+      >
+        <Image
+          src="/test-answer.png"
+          alt="law-gpt answering question"
+          width={320}
+          height={320}
+          className="absolute top-28 sm:top-32 md:top-40 left-10 sm:left-20 md:left-14 w-36 sm:w-52 md:w-80 transition-all duration-700 ease-out transform hover:scale-110"
+        />
+      </motion.div>
+    </div>
+  );
+};
+
+const MainTitle = () => {
+  const variants = {
+    hidden: { opacity: 0 },
+    visible: { opacity: 1 },
+  };
+
+  return (
+    <motion.h2
+      initial="hidden"
+      animate="visible"
+      variants={variants}
+      transition={{ duration: 1 }}
+      className="text-3xl sm:text-4xl md:text-6xl font-bold mb-10"
+    >
+      <span>당신을 위한 </span>
+      <br />
+      <span>빠른 무료법률상담 서비스</span>
+    </motion.h2>
+  );
+};
+
+const MainIntro = () => {
+  const variants = {
+    hidden: { opacity: 0 },
+    visible: { opacity: 1 },
+  };
+
+  return (
+    <motion.div
+      initial="hidden"
+      animate="visible"
+      variants={variants}
+      transition={{ duration: 1 }}
+      className="text-xl md:text-3xl mt-28 mb-48"
+    >
+      <div>
+        법률서비스 이용자들에게 <strong>생성형AI</strong>를 이용한
+      </div>
+      <div>무료법률상담 솔루션을 제공하고</div>
+      <div>이를 바탕으로 전문 변호사의 정보를 제공합니다.</div>
+    </motion.div>
+  );
+};
