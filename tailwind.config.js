a3242ba7ed9f524e9978c940ed02195feb76582b
youjin-10 <105618795+youjin-10@users.noreply.github.com>
2023-04-08 21:54:23 +0900

    add custom animation, keyframes

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,17 @@
 module.exports = {
   content: ["./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      animation: {
+        blink: "blink 2s linear infinite",
+      },
+      keyframes: {
+        blink: {
+          "0%, 100%": { opacity: 1 },
+          "50%": { opacity: 0.8 },
+        },
+      },
+    },
   },
   plugins: [],
 };
