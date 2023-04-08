aca759fbeb872ffbda48455f4fbe5a4d2848c4ff
youjin-10 <105618795+youjin-10@users.noreply.github.com>
2023-04-08 10:54:34 +0900

    kakao sdk

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -4,7 +4,13 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head>
-        <link rel="icon" href="/favicon.ico" />
+        <link rel="icon" href="/favicon_lawgpt.ico" />
+        <script
+          async
+          src="https://t1.kakaocdn.net/kakao_js_sdk/2.1.0/kakao.min.js"
+          integrity="sha384-dpu02ieKC6NUeKFoGMOKz6102CLEWi9+5RQjWSV0ikYSFFd8M3Wp2reIcquJOemx"
+          crossOrigin="anonymous"
+        ></script>
       </Head>
       <body>
         <Main />
