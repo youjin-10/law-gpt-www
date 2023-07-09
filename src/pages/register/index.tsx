0d3d75f1f676911e52994ab841c1d18e67ff93fb
youjin-10 <105618795+youjin-10@users.noreply.github.com>
2023-07-09 21:33:32 +0900

    update: 가입 버튼 누르면 일단 메인 이동

--- src/pages/register/index.tsx
+++ src/pages/register/index.tsx
@@ -1,6 +1,11 @@
+import { useRouter } from "next/router";
 import React from "react";
 
 const Index = () => {
+  const router = useRouter();
+  const handleRegister = () => {
+    router.replace("/");
+  };
   return (
     <section>
       <h2 className="text-2xl font-bold text-center my-20">
@@ -67,7 +72,10 @@ const Index = () => {
             className="w-full border border-gray-300 rounded-md bg-transparent p-1.5 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6"
           />
         </div>
-        <button className="rounded-md bg-purple-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-purple-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-purple-600">
+        <button
+          onClick={handleRegister}
+          className="rounded-md bg-purple-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-purple-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-purple-600"
+        >
           회원가입
         </button>
       </form>
